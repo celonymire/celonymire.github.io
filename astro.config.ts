f69e4c39c28abe37ef43c9766d4f2a153934378f
Sam Huang <samestimable2016@gmail.com>
2025-11-03 12:39:40 -0800

    Migrate Astro config file to TypeScript

--- astro.config.ts
+++ astro.config.ts
@@ -1,5 +1,3 @@
-// @ts-check
-
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
@@ -7,11 +5,8 @@ import { defineConfig } from "astro/config";
 
 export default defineConfig({
     site: "https://celonymire.github.io",
-    redirects: {
-        "/": "/avatar-cam",
-    },
     integrations: [mdx(), sitemap()],
     vite: {
-        plugins: [tailwindcss()],
+        plugins: [tailwindcss() as any], // FIXME: TailwindCSS giving type issues
     },
 });
