539e8ed293f9e52f51298c1d3e926e1bce0d8921
Sam Huang <samestimable2016@gmail.com>
2025-11-04 13:40:13 -0800

    More WIP: Added initial navigation menu

---
+++ src/lib/constants.ts
@@ -0,0 +1,14 @@
+export const PAGES = [
+    {
+        title: "Home",
+        link: "/",
+    },
+    {
+        title: "About",
+        link: "/about",
+    },
+    {
+        title: "Avatar Cam",
+        link: "/avatar-cam",
+    },
+] as const;
